f2f06e9e4528d1b055dc6c008239f76a453dcc9f
chauhanvats3 <chauhan.vats3@gmail.com>
2019-03-01 01:24:09 +0530

    Idk

--- app/src/main/java/com/dazedconfused/WallPix/MainActivity.java
+++ app/src/main/java/com/dazedconfused/WallPix/MainActivity.java
@@ -1,11 +1,14 @@
 package com.dazedconfused.WallPix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.lang.ref.WeakReference;
 
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private static WeakReference<MainActivity> activityWeakReference;
     private ImageView mainImage;
     private EditText editTextSearchQuery;
+    private DrawerLayout drawerLayout;
+    private int backPresses;
 
     public static MainActivity getMInstanceActivityContext() {
         return activityWeakReference.get();
@@ -39,7 +44,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
         activityWeakReference = new WeakReference<>(this);
+        backPresses = 0;
 
         //Required Objects :
         mainImage = findViewById(R.id.imgMainImage);
@@ -52,4 +59,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else if (backPresses == 0) {
+            Toast.makeText(this, "Press Back Again To Close!", Toast.LENGTH_SHORT).show();
+            backPresses++;
+        } else {
+            backPresses=0;
+            super.onBackPressed();
+        }
+    }
 }
